53e0fab4672be409e0fb7c0874a3ae8abd625c13
shabiga sahadevan <166649770+shabiga@users.noreply.github.com>
2024-05-16 20:05:58 -0400

    Update homework_2.sql

--- 03_homework/homework_2.sql
+++ 03_homework/homework_2.sql
@@ -2,7 +2,6 @@
 /* 1. Write a query that returns everything in the customer table. */
 SELECT * FROM customer; 
 
-
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
 SELECT * FROM customer
